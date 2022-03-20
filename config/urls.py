5940f63d139f8f2b856978a939c2d4fd30c34894
Masato Nagata <nagatamasato1986@gmail.com>
2022-03-20 22:51:34 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -9,5 +9,3 @@ urlpatterns = [
     path('<int:num>/', top, name='top'),
     path('analysis/', include('analysis.urls')),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
